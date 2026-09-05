90dcf669d35e8434a522cec63470bd00c83b6d97
krishnaritishsanka <krishnaritishsanka@gmail.com>
2026-09-05 10:40:20 +0530

    Added

---
+++ WEEK4(ASSIG)/SubarraySumEqualsK.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+
+public class SubarraySumEqualsK {
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int currentSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            currentSum += nums[i];
+
+            if (map.containsKey(currentSum - k)) {
+                count += map.get(currentSum - k);
+            }
+
+            map.put(
+                currentSum,
+                map.getOrDefault(currentSum, 0) + 1
+            );
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        System.out.println(subarraySum(nums, k));
+    }
+}
